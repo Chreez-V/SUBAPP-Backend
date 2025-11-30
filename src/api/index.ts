6bd90099915bf345a00f017df7f44d0d59fca49d
Zack20j <joeydaniels291202@gmail.com>
2025-11-30 14:49:34 -0400

    feat(auth): implement JWT authentication and login routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { healthRoutes } from "./health.routes";
 import { databaseRoutes } from "./database.routes";
+import { authRoutes } from "./auth/login";
+import jwtPlugin from "@/plugins/jwtPlugin";
 
 export async function routes(fastify: FastifyInstance) {
   await fastify.register(healthRoutes);
   await fastify.register(databaseRoutes);
+  
+  await fastify.register(jwtPlugin);
+  await fastify.register(authRoutes, { prefix: '/auth' });
 }
