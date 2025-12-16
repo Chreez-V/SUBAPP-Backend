3e6d6f7c84223b2caf0a58ab582b47624e56ba5a
Chreez <christian.ing.vqz@gmail.com>
2025-12-16 18:35:41 -0400

    Refactor modules

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 console.log("test");
+export {};
 //# sourceMappingURL=index.js.map
--- dist/server/app.js
+++ dist/server/app.js
@@ -1,17 +1,11 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = main;
-const mongoose_1 = __importDefault(require("mongoose"));
-require("dotenv/config");
-const env_config_1 = require("@/config/env.config");
-const app_config_1 = require("@/config/app.config");
+import mongoose from "mongoose";
+import "dotenv/config";
+import { envs } from "@/config/env.config";
+import { app } from "@/config/app.config";
 // TODO: Move this to a database config file
 const connectDB = async () => {
     try {
-        await mongoose_1.default.connect(env_config_1.envs.MONGODB_URL);
+        await mongoose.connect(envs.MONGODB_URL);
         console.log("✅ Connected to MongoDB Atlas");
     }
     catch (error) {
@@ -19,16 +13,16 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-async function main() {
+export async function main() {
     try {
         await connectDB();
-        const server = await (0, app_config_1.app)();
+        const server = await app();
         await server.listen({
-            port: env_config_1.envs.PORT || 3500,
-            host: env_config_1.envs.HOST || "0.0.0.0"
+            port: envs.PORT || 3500,
+            host: envs.HOST || "0.0.0.0"
         });
-        console.log(`✅ Swagger docs available at http://localhost:${env_config_1.envs.PORT || 3500}/docs`);
-        console.log(`Server ready at http://localhost:${env_config_1.envs.PORT || 3500}`);
+        console.log(`✅ Swagger docs available at http://localhost:${envs.PORT || 3500}/docs`);
+        console.log(`Server ready at http://localhost:${envs.PORT || 3500}`);
     }
     catch (e) {
         const errorMessage = e instanceof Error ? e.message : 'Unknown error occurred';
