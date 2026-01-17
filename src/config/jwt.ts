c54cc06abf741bb70872fcd4343196df77158c64
Anyelis-Coro <anyeliscoro@gmail.com>
2026-01-17 15:17:06 -0400

    feat: agregar middleware de autenticación admint-13

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -2,12 +2,35 @@ import fp from 'fastify-plugin';
 import fastifyJwt from '@fastify/jwt';
 import { FastifyReply, FastifyRequest } from "fastify";
 import { envs } from "./env.config.js";
+import { User } from "../models/user.js";
 
 export default fp(async (fastify, _opts) => {
   fastify.register(fastifyJwt, {
     secret: envs.JWT_SECRET
   });
 
+ fastify.decorate('authenticateAdmin', async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+       const payload = request.user as { email: string; role: string };
+    
+    const user = await User.findOne({ 
+      email: payload.email, 
+      role: "admin" 
+    });
+
+     if (!user) {
+        reply.code(403).send({ 
+          error: "Forbidden", 
+          message: "No tienes permisos de administrador" 
+        });
+        return; // Importante: detener ejecución
+      }
+
+    } catch (err) {
+      reply.send(err);
+    }
+  });
   fastify.decorate('authenticate', async (request: FastifyRequest, reply:FastifyReply) => {
     try {
       await request.jwtVerify();
@@ -15,5 +38,6 @@ export default fp(async (fastify, _opts) => {
       reply.send(err);
     }
   });
+
 });
 
