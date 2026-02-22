a98eb692a6ee6512e937c9b9dcca861aca6d1a59
Scipear <Pantauwu09@gmail.com>
2026-02-22 04:19:48 -0400

    fix: typing errors

--- src/api/wallet/wallet.routes.ts
+++ src/api/wallet/wallet.routes.ts
@@ -10,10 +10,10 @@ import { rejectPaymentSchema } from '../../validators/wallet.schema.js'
 
 export async function walletRoutes(fastify: FastifyInstance) {
   // 1. Creamos el middleware
-  //const authenticate = createJwtMiddleware(fastify)
+  const authenticate = createJwtMiddleware(fastify)
 
   // 2. Usamos 'authenticate' como preHandler para proteger las rutas de billetera
-  //fastify.addHook('preHandler', authenticate)
+  fastify.addHook('preHandler', authenticate)
 
   // Endpoint: GET /api/wallet/saldo
   fastify.get('/saldo', getSaldo)
